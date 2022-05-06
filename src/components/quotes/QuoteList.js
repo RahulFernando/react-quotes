344f961d53beea1ca7ffe51a85b5d992822d61bd
RahulFernando <rahulrulz680@gmail.com>
2022-05-06 17:14:13 +0530

    asc and desc

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,13 +1,43 @@
 import { Fragment } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((a, b) => {
+    if (ascending) {
+      return a.id > b.id ? 1 : -1;
+    } else {
+      return a.id < b.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+
+  const isSortingAscending = queryParams.get('sort') === 'asc';
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  const changeSortHandler = () => {
+    history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'));
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortHandler}>
+          Sort {!isSortingAscending ? 'Ascending' : 'Descending'}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -3,9 +3,9 @@ import React from 'react';
 import QuoteList from '../components/quotes/QuoteList';
 
 const DUMMY_QUOTES = [
-  { id: 'q1', author: 'Max', text: 'Learning react is fun' },
-  { id: 'q2', author: 'Rahul', text: 'Learning flutter is fun' },
-  { id: 'q2', author: 'Ravan', text: 'Learning vanilla Js is fun' },
+  { id: 1, author: 'Max', text: 'Learning react is fun' },
+  { id: 2, author: 'Rahul', text: 'Learning flutter is fun' },
+  { id: 3, author: 'Ravan', text: 'Learning vanilla Js is fun' },
 ];
 
 const AllQuotes = () => {
